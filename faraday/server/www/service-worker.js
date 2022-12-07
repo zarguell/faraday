4eaf2c68231a06bfa436d683619b126a4eafb557
Automated <actions@users.noreply.github.com>
2022-12-07 04:34:36 +0000

    Sync faraday: Wed Dec  7 04:34:36 UTC 2022

--- faraday/server/www/service-worker.js
+++ faraday/server/www/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7aa0d97d558ef0bbbd913bdb2fde3bbb.js"
+  "/precache-manifest.855704232cc6c95da1636429743ce2b2.js"
 );
 
 self.addEventListener('message', (event) => {
